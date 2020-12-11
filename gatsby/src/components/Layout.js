1ca59ab22c70c9296438cb0ec14dc05b18cf0ced
Brad Fabian <bradleofabian@gmail.com>
2020-12-11 09:26:31 -0500

    added logo, navbar and type

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -3,11 +3,13 @@ import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
 import 'normalize.css';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
+      <Typography />
       <Nav />
       <p>{children}</p>
       <Footer />
